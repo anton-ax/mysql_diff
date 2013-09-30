378806b968f869cc5ebf76dfb7859d25a674550a
Anton <native1989@gmail.com>
2013-09-30 19:36:30 +0500

    Update compare.php

--- bin/compare.php
+++ bin/compare.php
@@ -15,7 +15,7 @@ $params = array(
 
 $options = getopt(implode('', array_keys($params)), $params);
 
-if( $options["from"] || !$options["to"] || !$options["alter"]) {
+if( !$options["from"] || !$options["to"] || !$options["alter"]) {
 	die ("Use: ./bin/compare.php --from=user1:pass1@host1:3306/db1 --to=user2:pass2@host2:3307/db2 --alter=/tmp/alter.sql\n");
 }
 $dumpFrom = new Dumper($options["from"]);
